f9738580978d12cb24fd031c5ba5cb0c829c16cc
WuZixing <wzixing@qq.com>
2021-11-10 15:06:06 +0800

    text

--- OpenGeo3D/Text.cpp
+++ OpenGeo3D/Text.cpp
@@ -2,15 +2,15 @@
 #include <QtGui/QKeySequence>
 
 QString Text::appName() {
-	return QString(APP_NAME);
+	return QStringLiteral(APP_NAME);
 }
 
 QString Text::appVersion() {
-	return QString(APP_VERSION);
+	return QStringLiteral(APP_VERSION);
 }
 
 QString Text::appDescription() {
-	return appName() + QString::fromUtf8("是一个开源的、以网格为基础的三维地质建模工具。");
+	return appName() + QStringLiteral("是一个开源的、以网格为基础的三维地质建模工具。");
 }
 
 QString Text::appWebSite() {
@@ -18,331 +18,331 @@ QString Text::appWebSite() {
 }
 
 QString Text::nameOfStructureModel() {
-	return QString::fromUtf8("结构模型");
+	return QStringLiteral("结构模型");
 }
 
 QString Text::nameOfVoxelGridModel() {
-	return QString::fromUtf8("网格模型");
+	return QStringLiteral("网格模型");
 }
 
 QString Text::nameOfDefaultG3DMap() {
-	return QString::fromUtf8("默认场景");
+	return QStringLiteral("默认场景");
 }
 
 QString Text::nameOfClassUnknown() {
-	return QString::fromUtf8("Unknown");
+	return QStringLiteral("Unknown");
 }
 
 QString Text::nameOfClassG3DPoint() {
-	return QString::fromUtf8("Point");
+	return QStringLiteral("Point");
 }
 
 QString Text::nameOfClassG3DMPoint() {
-	return QString::fromUtf8("Multiple Point");
+	return QStringLiteral("Multiple Point");
 }
 
 QString Text::nameOfClassG3DAnnotation() {
-	return QString::fromUtf8("Annotation");
+	return QStringLiteral("Annotation");
 }
 
 QString Text::nameOfClassG3DLineString() {
-	return QString::fromUtf8("Line String");
+	return QStringLiteral("Line String");
 }
 
 QString Text::nameOfClassG3DTIN() {
-	return QString::fromUtf8("TIN");
+	return QStringLiteral("TIN");
 }
 
 QString Text::nameOfClassG3DUniformGrid() {
-	return QString::fromUtf8("Uniform Grid");
+	return QStringLiteral("Uniform Grid");
 }
 
 QString Text::nameOfClassG3DCornerGrid() {
-	return QString::fromUtf8("Corner Grid");
+	return QStringLiteral("Corner Grid");
 }
 
 QString Text::nameOfRepresentationModePoints() {
-	return QString::fromUtf8("顶点");
+	return QStringLiteral("顶点");
 }
 
 QString Text::nameOfRepresentationModeWireframe() {
-	return QString::fromUtf8("线框");
+	return QStringLiteral("线框");
 }
 
 QString Text::nameOfRepresentationModeSurface() {
-	return QString::fromUtf8("面");
+	return QStringLiteral("面");
 }
 
 QString Text::nameOfDrillModel() {
-	return QString::fromUtf8("钻孔");
+	return QStringLiteral("钻孔");
 }
 
 QString Text::nameOfDrillFeatureClass() {
-	return QString::fromUtf8("钻孔");
+	return QStringLiteral("钻孔");
 }
 
 QString Text::nameOfDrillPositionFeature() {
-	return QString::fromUtf8("钻孔位置");
+	return QStringLiteral("钻孔位置");
 }
 
 QString Text::menuFile() {
-	return QString::fromUtf8("文件(&F)");
+	return QStringLiteral("文件(&F)");
 }
 
 QString Text::menuStructureModel() {
-	return QString::fromUtf8("结构模型");
+	return QStringLiteral("结构模型");
 }
 
 QString Text::menuOpenGeo3DML() {
-	return QString::fromUtf8("打开Geo3DML文件...");
+	return QStringLiteral("打开Geo3DML文件...");
 }
 
 QString Text::menuOpenDrillLog() {
-	return QString::fromUtf8("打开简单钻孔记录...");
+	return QStringLiteral("打开简单钻孔记录...");
 }
 
 QString Text::menuGridModel() {
-	return QString::fromUtf8("网格模型");
+	return QStringLiteral("网格模型");
 }
 
 QString Text::menuCloseStructureModel() {
-	return QString::fromUtf8("关闭结构模型");
+	return QStringLiteral("关闭结构模型");
 }
 
 QString Text::menuCloseAllModels() {
-	return QString::fromUtf8("关闭所有数据");
+	return QStringLiteral("关闭所有数据");
 }
 
 QString Text::menuHelp() {
-	return QString::fromUtf8("帮助(&H)");
+	return QStringLiteral("帮助(&H)");
 }
 
 QString Text::menuAbout() {
-	return QString::fromUtf8("关于(&A)...");
+	return QStringLiteral("关于(&A)...");
 }
 
 QString Text::menuQuit() {
-	return QString::fromUtf8("退出(&Q)");
+	return QStringLiteral("退出(&Q)");
 }
 
 QString Text::menuWindow() {
-	return QString::fromUtf8("窗口(&W)");
+	return QStringLiteral("窗口(&W)");
 }
 
 QString Text::menuFullView() {
-	return QString::fromUtf8("全景(&R)\t") + QKeySequence(Qt::Modifier::CTRL + Qt::Key::Key_R).toString();
+	return QStringLiteral("全景(&R)\t") + QKeySequence(Qt::Modifier::CTRL + Qt::Key::Key_R).toString();
 }
 
 QString Text::menuBackgroundColor() {
-	return QString::fromUtf8("背景颜色(&B)\t") + QKeySequence(Qt::Modifier::CTRL + Qt::Key::Key_B).toString();
+	return QStringLiteral("背景颜色(&B)\t") + QKeySequence(Qt::Modifier::CTRL + Qt::Key::Key_B).toString();
 }
 
 QString Text::menuScaleZUp() {
-	return QString::fromUtf8("Z轴拉伸(&U)\t") + QKeySequence(Qt::Modifier::CTRL + Qt::Key::Key_Up).toString();
+	return QStringLiteral("Z轴拉伸(&U)\t") + QKeySequence(Qt::Modifier::CTRL + Qt::Key::Key_Up).toString();
 }
 
 QString Text::menuScaleZDown() {
-	return QString::fromUtf8("Z轴压缩(&D)\t") + QKeySequence(Qt::Modifier::CTRL + Qt::Key::Key_Down).toString();
+	return QStringLiteral("Z轴压缩(&D)\t") + QKeySequence(Qt::Modifier::CTRL + Qt::Key::Key_Down).toString();
 }
 
 QString Text::menuCustomizedZScale() {
-	return QString::fromUtf8("自定义Z轴缩放...");
+	return QStringLiteral("自定义Z轴缩放...");
 }
 
 QString Text::menuResetZScale() {
-	return QString::fromUtf8("取消Z轴缩放");
+	return QStringLiteral("取消Z轴缩放");
 }
 
 QString Text::menuProjectPanel() {
-	return QString::fromUtf8("模型列表");
+	return QStringLiteral("模型列表");
 }
 
 QString Text::menuSaveToGeo3DML() {
-	return QString::fromUtf8("保存为Geo3DML文件...");
+	return QStringLiteral("保存为Geo3DML文件...");
 }
 
 QString Text::titleOfDlgAbout() {
-	return QString::fromUtf8("关于") + appName();
+	return QStringLiteral("关于") + appName();
 }
 
 QString Text::titleOfConfirmingQuit() {
-	return QString::fromUtf8("退出") + appName();
+	return QStringLiteral("退出") + appName();
 }
 
 QString Text::titleOfProjectPanel() {
-	return QString::fromUtf8("模型");
+	return QStringLiteral("模型");
 }
 
 QString Text::titleOfMetadataPage() {
-	return QString::fromUtf8("数据");
+	return QStringLiteral("数据");
 }
 
 QString Text::titleOfRenderOptionPage() {
-	return QString::fromUtf8("可视化");
+	return QStringLiteral("可视化");
 }
 
 QString Text::confirmToCloseStructureModel() {
-	return QString::fromUtf8("确定关闭结构模型吗？未保存的数据将会丢失。");
+	return QStringLiteral("确定关闭结构模型吗？未保存的数据将会丢失。");
 }
 
 QString Text::confirmToCloseAllModels() {
-	return QString::fromUtf8("确定关闭所有模型吗？未保存的数据将会丢失。");
+	return QStringLiteral("确定关闭所有模型吗？未保存的数据将会丢失。");
 }
 
 QString Text::confirmToQuit() {
-	return QString::fromUtf8("确定退出") + appName() + QString::fromUtf8("吗？未保存的数据将会丢失。");
+	return QStringLiteral("确定退出") + appName() + QStringLiteral("吗？未保存的数据将会丢失。");
 }
 
 QString Text::filterOfGeo3DMLFile() {
-	return QString::fromUtf8("Geo3DML文件（UTF-8编码）(*.xml)");
+	return QStringLiteral("Geo3DML文件（UTF-8编码）(*.xml)");
 }
 
 QString Text::filterOfGeo3DMLFileWithVersion() {
-	return QString::fromUtf8("Geo3DML（v1.x）文件(*.xml);;Geo3DML（v1.0）文件(*.xml)");
+	return QStringLiteral("Geo3DML（v1.x）文件(*.xml);;Geo3DML（v1.0）文件(*.xml)");
 }
 
 QString Text::filterOfDrillPositionFile() {
-	return QString::fromUtf8("钻孔位置(*.csv;*.prn)");
+	return QStringLiteral("钻孔位置(*.csv;*.prn)");
 }
 
 QString Text::filterOfDrillLogFile() {
-	return QString::fromUtf8("钻孔测试数据(*.csv;*.prn)");
+	return QStringLiteral("钻孔测试数据(*.csv;*.prn)");
 }
 
 QString Text::filterOfShpFile() {
-	return QString::fromUtf8("Shape文件(*.shp)");
+	return QStringLiteral("Shape文件(*.shp)");
 }
 
 QString Text::labelOfBasicMetaInfo() {
-	return QString::fromUtf8("基本信息");
+	return QStringLiteral("基本信息");
 }
 
 QString Text::labelOfDataset() {
-	return QString::fromUtf8("数据集");
+	return QStringLiteral("数据集");
 }
 
 QString Text::labelOfSubNodeNumber() {
-	return QString::fromUtf8("子节点数");
+	return QStringLiteral("子节点数");
 }
 
 QString Text::labelOfId() {
-	return QString::fromUtf8("ID");
+	return QStringLiteral("ID");
 }
 
 QString Text::labelOfAABB() {
-	return QString::fromUtf8("最小外包矩形");
+	return QStringLiteral("最小外包矩形");
 }
 
 QString Text::labelOfAABBMinPoint() {
-	return QString::fromUtf8("最小坐标");
+	return QStringLiteral("最小坐标");
 }
 
 QString Text::labelOfAABBMaxPoint() {
-	return QString::fromUtf8("最大坐标");
+	return QStringLiteral("最大坐标");
 }
 
 QString Text::labelOfStructureModel() {
-	return QString::fromUtf8("结构模型");
+	return QStringLiteral("结构模型");
 }
 
 QString Text::labelOf3DMap() {
-	return QString::fromUtf8("地质图/模型");
+	return QStringLiteral("地质图/模型");
 }
 
 QString Text::labelOfLayer() {
-	return QString::fromUtf8("图层/要素类");
+	return QStringLiteral("图层/要素类");
 }
 
 QString Text::labelOfActor() {
-	return QString::fromUtf8("图元/要素");
+	return QStringLiteral("图元/要素");
 }
 
 QString Text::labelOfFeatureClass() {
-	return QString::fromUtf8("要素类");
+	return QStringLiteral("要素类");
 }
 
 QString Text::labelOfFeature() {
-	return QString::fromUtf8("要素");
+	return QStringLiteral("要素");
 }
 
 QString Text::labelOfName() {
-	return QString::fromUtf8("名字");
+	return QStringLiteral("名字");
 }
 
 QString Text::labelOfSchema() {
-	return QString::fromUtf8("属性表结构");
+	return QStringLiteral("属性表结构");
 }
 
 QString Text::labelOfGeometry() {
-	return QString::fromUtf8("几何对象");
+	return QStringLiteral("几何对象");
 }
 
 QString Text::labelOfLOD() {
-	return QString::fromUtf8("LOD");
+	return QStringLiteral("LOD");
 }
 
 QString Text::labelOfClassName() {
-	return QString::fromUtf8("数据类型");
+	return QStringLiteral("数据类型");
 }
 
 QString Text::labelOfNumberOfVertices() {
-	return QString::fromUtf8("顶点数");
+	return QStringLiteral("顶点数");
 }
 
 QString Text::labelOfNumberOfTriangles() {
-	return QString::fromUtf8("三角形数");
+	return QStringLiteral("三角形数");
 }
 
 QString Text::labelOfGridOrigin() {
-	return QString::fromUtf8("网格原点");
+	return QStringLiteral("网格原点");
 }
 
 QString Text::labelOfGridCellSize() {
-	return QString::fromUtf8("网格单元大小");
+	return QStringLiteral("网格单元大小");
 }
 
 QString Text::labelOfGridCellDimension() {
-	return QString::fromUtf8("网格单元数目");
+	return QStringLiteral("网格单元数目");
 }
 
 QString Text::labelOfRenderOption() {
-	return QString::fromUtf8("绘制参数");
+	return QStringLiteral("绘制参数");
 }
 
 QString Text::labelOfColor() {
-	return QString::fromUtf8("颜色");
+	return QStringLiteral("颜色");
 }
 
 QString Text::labelOfRepresentationMode() {
-	return QString::fromUtf8("绘制方式");
+	return QStringLiteral("绘制方式");
 }
 
 QString Text::labelOfDrawEdge() {
-	return QString::fromUtf8("绘制线框");
+	return QStringLiteral("绘制线框");
 }
 
 QString Text::tipOfCustomizedZScale() {
-	return QString::fromUtf8("请输入Z轴缩放系数（大于0，且小于1000）：");
+	return QStringLiteral("请输入Z轴缩放系数（大于0，且小于1000）：");
 }
 
 QString Text::labelOfSimpleDrillLogGroup() {
-	return QString::fromUtf8("钻孔数据");
+	return QStringLiteral("钻孔数据");
 }
 
 QString Text::labelOfDrillPositionFilePath() {
-	return QString::fromUtf8("钻孔位置");
+	return QStringLiteral("钻孔位置");
 }
 
 QString Text::labelOfSavingDrillPositionToFile() {
-	return QString::fromUtf8("保存钻孔位置到Shape文件");
+	return QStringLiteral("保存钻孔位置到Shape文件");
 }
 
 QString Text::labelOfOpen() {
-	return QString::fromUtf8("打开...");
+	return QStringLiteral("打开...");
 }
 
 QString Text::labelOfSave() {
-	return QString::fromUtf8("保存...");
+	return QStringLiteral("保存...");
 }
 
 QString Text::labelOfNo() {
@@ -350,11 +350,11 @@ QString Text::labelOfNo() {
 }
 
 QString Text::labelOfDrillNo() {
-	return QString::fromUtf8("钻孔编号");
+	return QStringLiteral("钻孔编号");
 }
 
 QString Text::labelOfDrillDepth() {
-	return QString::fromUtf8("孔深");
+	return QStringLiteral("孔深");
 }
 
 QString Text::labelOfX() {
@@ -370,69 +370,69 @@ QString Text::labelOfZ() {
 }
 
 QString Text::labelOfDrillLog() {
-	return QString::fromUtf8("钻孔测试记录");
+	return QStringLiteral("钻孔测试记录");
 }
 
 QString Text::labelOfAppend() {
-	return QString::fromUtf8("添加...");
+	return QStringLiteral("添加...");
 }
 
 QString Text::labelOfClear() {
-	return QString::fromUtf8("清除");
+	return QStringLiteral("清除");
 }
 
 QString Text::labelOfFilePath() {
-	return QString::fromUtf8("文件路径");
+	return QStringLiteral("文件路径");
 }
 
 QString Text::labelOfField() {
-	return QString::fromUtf8("属性字段");
+	return QStringLiteral("属性字段");
 }
 
 QString Text::labelOfFieldValueType() {
-	return QString::fromUtf8("数据类型");
+	return QStringLiteral("数据类型");
 }
 
 QString Text::labelOfCRS() {
-	return QString::fromUtf8("坐标参照系");
+	return QStringLiteral("坐标参照系");
 }
 
 QString Text::tipOfSpecifyFieldValueType() {
-	return QString::fromUtf8("请为属性字段指定适当的“数据类型”。");
+	return QStringLiteral("请为属性字段指定适当的“数据类型”。");
 }
 
 QString Text::errorOfOpenFile(const QString& filePath, const QString& errorMessage) {
-	return QString::fromUtf8("打开文件 %1 失败：\r\n%2").arg(filePath, errorMessage);
+	return QStringLiteral("打开文件 %1 失败：\r\n%2").arg(filePath, errorMessage);
 }
 
 QString Text::confirmToClearFileList() {
-	return QString::fromUtf8("确认清空文件列表吗？");
+	return QStringLiteral("确认清空文件列表吗？");
 }
 
 QString Text::tipOfEmptyDrillPosition() {
-	return QString::fromUtf8("请指定钻孔位置文件。");
+	return QStringLiteral("请指定钻孔位置文件。");
 }
 
 QString Text::tipOfEmptyDrillPositionShpFilePath() {
-	return QString::fromUtf8("请指定保存钻孔位子的Shape文件的路径。");
+	return QStringLiteral("请指定保存钻孔位子的Shape文件的路径。");
 }
 
 QString Text::tipOfEmptyDrillLogFiles() {
-	return QString::fromUtf8("请指定钻孔测试记录数据。");
+	return QStringLiteral("请指定钻孔测试记录数据。");
 }
 
 QString Text::tipOfEmptyCRS() {
-	return QString::fromUtf8("请指定坐标参照系。");
+	return QStringLiteral("请指定坐标参照系。");
 }
 
 QString Text::tipOfUnknownFieldValueType() {
-	return QString::fromUtf8("请指定属性字段的数据类型。");
+	return QStringLiteral("请指定属性字段的数据类型。");
 }
 
 QString Text::tipOfSucceedInSavingToGeo3DMLFile(const QString& modelName) {
-	return QString::fromUtf8("导出模型“%1”到Geo3DML文件成功。").arg(modelName);
+	return QStringLiteral("导出模型“%1”到Geo3DML文件成功。").arg(modelName);
 }
 
 QString Text::tipOfErrorInSavingToGeo3DMLFile(const QString& modelName, const QString& error) {
-	return QString::fromUtf8("导出模型“%1”到Geo3DML文件出错：%2").arg(modelName).arg(error);
+	return QStringLiteral("导出模型“%1”到Geo3DML文件出错：%2").arg(modelName).arg(error);
 }
